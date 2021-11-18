e10069b76e97c1fc53ea5c6cf66f0b49a7a2d43d
harsh <harshanranges3@gmail.com>
2021-11-18 18:12:23 +0530

    Corrected Failed Test Case

--- src/test/java/WM/FreshWorksTest.java
+++ src/test/java/WM/FreshWorksTest.java
@@ -23,80 +23,45 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FreshWorksTest {
-	WebDriver driver;
+    WebDriver driver;
 
-	@BeforeMethod
-	@Parameters("browser")
-	public void setup(String browser) {
-		if (browser.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			// DesiredCapabilities cap = new DesiredCapabilities();
-			// cap.setCapability("browserName", "chrome");
-			// try {
-			// driver = new RemoteWebDriver(new
-			// URL("http://13.233.116.37:4444/wd/hub"), cap);
-			// } catch (MalformedURLException e) {
-			// e.printStackTrace();
-			// }
-		}
+    @BeforeMethod
+    @Parameters("browser")
+    public void setup(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://google.com/");
 
-		else if (browser.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			// DesiredCapabilities cap = new DesiredCapabilities();
-			// cap.setCapability("browserName", "firefox");
-			// try {
-			// driver = new RemoteWebDriver(new
-			// URL("http://13.233.116.37:4444/wd/hub"), cap);
-			// } catch (MalformedURLException e) {
-			// e.printStackTrace();
-			// }
+    }
 
-		}
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.get("https://google.com/ncr");
+    @Test(priority = 1)
+    public void GoogleSearchTest1() {
 
-	}
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+        WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+        assertEquals(driver.getTitle(), "cheese - Google Search");
+    }
 
-	@Test(priority = 1)
-	public void GoogleSearchTest1() {
+    @Test(priority = 2)
+    public void GoogleSearchTest2() {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
-		WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
-		assertEquals(driver.getTitle(), "cheese - Google Search");
-		System.out.println(firstResult.getAttribute("1st testcase"));
-	}
-
-	@Test(priority = 1)
-	public void GoogleSearchTest2() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		driver.findElement(By.name("q")).sendKeys("tomato" + Keys.ENTER);
-		WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
-		assertEquals(driver.getTitle(), "cheese - Google Search");
-		System.out.println(firstResult.getAttribute("1st testcase"));
-	}
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.name("q")).sendKeys("tomato" + Keys.ENTER);
+        WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+        assertEquals(driver.getTitle(), "tomato - Google Search");
+    }
 
 
-//	@Test(priority = 2)
-//	public void freshWorksTitleTest() {
-//		System.out.println("running title test...");
-//		System.out.println(driver.getTitle());
-//		assertEquals(driver.getTitle(), "A fresh approach to customer engagement");
-//	}
-//
-//	@Test(priority = 3)
-//	public void getFooterLinksTest() {
-//		List<WebElement> footerLinksList = driver.findElements(By.cssSelector("ul.footer-nav li a"));
-//		footerLinksList.forEach(ele -> System.out.println(ele.getText()));
-//		assertEquals(footerLinksList.size(), 35);
-//	}
-
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
-	}
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
